f9b81fb30d2abb9b127823a259085b579b985dcd
mwalcher3 <melisande.walcher@gmail.com>
2022-06-04 17:58:00 +0200

    hello

---
+++ src/api/statute-page/services/statute-page.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * statute-page service.
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::statute-page.statute-page');
