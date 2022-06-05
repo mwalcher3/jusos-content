62e91d7a41af2277fcc1cc70b54ae8be267450f6
mwalcher3 <melisande.walcher@gmail.com>
2022-06-05 08:56:49 +0200

    hello

--- config/admin.js
+++ config/admin.js
@@ -1,6 +1,6 @@
 module.exports = ({ env }) => ({
   auth: {
-    secret: env('ADMIN_JWT_SECRET', '19b3c8c7776793812daf9d4b633cf5ac'),
+    secret: env('ADMIN_JWT_SECRET', '6CnV3xUJbRlozPZuC2jNe/by2weRLWP2tu7e5RCvTX0='),
   },
   apiToken: {
     salt: env('API_TOKEN_SALT'),
