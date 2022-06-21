d59bd6870059b211d2dfc3a1dd1f8ebed1b3eb64
mwalcher3 <melisande.walcher@gmail.com>
2022-06-21 19:43:26 +0200

    hello

--- src/api/calender-page/controllers/calender-page.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- *  calender-page controller
- */
-
-const { createCoreController } = require('@strapi/strapi').factories;
-
-module.exports = createCoreController('api::calender-page.calender-page');
--- src/api/calender-page/routes/calender-page.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * calender-page router.
- */
-
-const { createCoreRouter } = require('@strapi/strapi').factories;
-
-module.exports = createCoreRouter('api::calender-page.calender-page');
