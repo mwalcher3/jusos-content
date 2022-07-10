c0eef44d54c78a320e5cc69fec61afa06d6b84a4
mwalcher3 <melisande.walcher@gmail.com>
2022-07-10 14:14:29 +0200

    change in contact populate fetching

--- src/api/contact-page/controllers/contact-page.js
+++ src/api/contact-page/controllers/contact-page.js
@@ -6,4 +6,18 @@
 
 const { createCoreController } = require('@strapi/strapi').factories;
 
-module.exports = createCoreController('api::contact-page.contact-page');
+module.exports = createCoreController('api::contact-page.contact-page', ({strapi}) =>({
+  async find(ctx) {
+    // some logic here
+    if (ctx.request.url== '/api/contact-page?populate=*'){
+      ctx.request.url= "/api/contact-page?populate[form][populate][0]=inputs"
+    }
+
+    const { data, meta } = await super.find(ctx);
+    // some more logic
+    console.log(ctx.query)
+
+    return { data, meta };
+  }
+
+}));
