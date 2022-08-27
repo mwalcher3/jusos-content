09cf62b9a5fb41d78aca1f177f00cccfae76ed04
mwalcher3 <melisande.walcher@gmail.com>
2022-08-27 02:00:50 +0200

    new current page alias to fetch

--- src/api/current-topic-page/controllers/current-topic-page.js
+++ src/api/current-topic-page/controllers/current-topic-page.js
@@ -9,17 +9,19 @@ const { createCoreController } = require('@strapi/strapi').factories;
 //module.exports = createCoreController('api::current-topic-page.current-topic-page');
 
 module.exports = createCoreController('api::current-topic-page.current-topic-page', ({strapi}) =>({
-    async findSlug(ctx) {
+    async find(ctx) {
       // some logic here
   
       const { data, meta } = await super.find(ctx);
       // some more logic
+      //
+      const currentTopics = data;
 
-      const slug= data.attributes.slug
-      console.log("this is the slug!!!! :)", slug)
+      //const slug= data.attributes.slug
+      //console.log("this is the slug!!!! :)", slug)
 
   
-      return { slug, meta };
+      return { currentTopics, meta };
     }
   
   }));
