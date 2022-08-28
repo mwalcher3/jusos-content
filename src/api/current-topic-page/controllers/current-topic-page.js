c4fb23677774d3d20465c6578481be1c3907572a
mwalcher3 <melisande.walcher@gmail.com>
2022-08-29 00:42:58 +0200

    hello

--- src/api/current-topic-page/controllers/current-topic-page.js
+++ src/api/current-topic-page/controllers/current-topic-page.js
@@ -8,21 +8,5 @@ const { createCoreController } = require('@strapi/strapi').factories;
 
 //module.exports = createCoreController('api::current-topic-page.current-topic-page');
 
-module.exports = createCoreController('api::current-topic-page.current-topic-page', ({strapi}) =>({
-    async find(ctx) {
-      // some logic here
-  
-      const { data, meta } = await super.find(ctx);
-      // some more logic
-      //
-      const currentTopics = data;
-
-      //const slug= data.attributes.slug
-      //console.log("this is the slug!!!! :)", slug)
-
-  
-      return { currentTopics, meta };
-    }
-  
-  }));
+module.exports = createCoreController('api::current-topic-page.current-topic-page');
   
