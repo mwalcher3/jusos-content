b3e3c00b9e9817bd7b5abc9d033d2acbbb3466af
jwalcher <walcher@uni-heidelberg.de>
2023-07-30 22:27:18 +0200

    contact page

--- src/api/contact-page/controllers/contact-page.js
+++ src/api/contact-page/controllers/contact-page.js
@@ -6,17 +6,19 @@
 
 const { createCoreController } = require('@strapi/strapi').factories;
 
-module.exports = createCoreController('api::contact-page.contact-page', ({strapi}) =>({
-  async find(ctx) {
-    // some logic here
-    if (ctx.request.url== '/api/contact-page?populate=*'){
-      ctx.request.url= "/api/contact-page?populate[form][populate][0]=inputs"
-    }
+// module.exports = createCoreController('api::contact-page.contact-page', ({strapi}) =>({
+//   async find(ctx) {
+//     // some logic here
+//     if (ctx.request.url== '/api/contact-page?populate=*'){
+//       ctx.request.url= "/api/contact-page?populate[form][populate][0]=inputs"
+//     }
 
-    const { data, meta } = await super.find(ctx);
-    // some more logic
+//     const { data, meta } = await super.find(ctx);
+//     // some more logic
 
-    return { data, meta };
-  }
+//     return { data, meta };
+//   }
 
-}));
+// }));
+
+module.exports = createCoreController('api::contact-page.contact-page');
